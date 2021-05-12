ec28cd9e6ab7c61a05d14c1b6ea0df9069faffd0
STSRD <71692468+STSRD@users.noreply.github.com>
2021-05-12 15:12:13 +0300

    Update Parser.cs

--- Parser.cs
+++ Parser.cs
@@ -3,80 +3,65 @@ using System.Collections.Generic;
 using System.Text;
 using Google.GData.Client;
 using Google.GData.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.GData.Client;
+using Google.GData.Extensions;
 using Google.GData.Spreadsheets;
 using System.IO;
 using Microsoft.VisualBasic.FileIO;
 
-namespace Практика
+namespace Practice
 {
     enum AttackType
     {
-        range,
-        mele
+        Range,
+        Melee
     };
 
-    enum shellType
+    enum ShellType
     {
-        nothing,
-        arrow,
-        bolt,
-        magic,
-        bomb,
+        Nothing,
+        Arrow,
+        Bolt,
+        Magic,
+        Bombshell,
 
-    };
-
-    enum bonus
-    {
-        add,
-        not
     };
 
     static class Parser
     {
-        static private AttackType convertStringToAttackType(string type) {
+        static private AttackType ConvertStringToAttackType(string type) {
             switch (type)
             {
                 case "Дальняя":
-                    return AttackType.range;
+                    return AttackType.Range;
                 case "Ближняя":
-                    return AttackType.mele;
+                    return AttackType.Melee;
                 default:
-                    return AttackType.mele;
+                    return AttackType.Melee;
             }
         }
 
-        static private shellType convertStringToShellType(string type)
+        static private ShellType ConvertStringToShellType(string type)
         {
             switch (type)
             {
                 case "Стрела":
-                    return shellType.arrow;
+                    return ShellType.Arrow;
                 case "Болт":
-                    return shellType.bolt;
+                    return ShellType.Bolt;
                 case "Мигия":
-                    return shellType.magic;
+                    return ShellType.Magic;
                 case "Бомба":
-                    return shellType.bomb;
+                    return ShellType.Bombshell;
                 default:
-                    return shellType.nothing;
+                    return ShellType.Nothing;
             }
         }
 
-        static private bonus convertStringToBonus(string type)
-        {
-            switch (type)
-            {
-                case "":
-                    return bonus.not;
-                default:
-                    return bonus.add;
-            }
-        }
-
-        static public Races ParserBuildings(string link) {
-            //возвращаемый массив
-            List<Building> building = new List<Building>();
-
+        static public Race ParseBuildings(string link) {
             //Чтение данных
             List<string[]> date = new List<string[]>();
             using (TextFieldParser tfp = new TextFieldParser(link)) {
@@ -90,125 +75,40 @@ namespace Практика
             }
 
             //Подсчет строений
-            int koll_buildings = date[0].Length / 13;
+            int numberOfBuildings = date[0].Length / 13;
 
-            //запись списка зданий рассы
+            //Запись списка зданий рассы
             List<Building> buildings = new List<Building>();
 
-            for (int i = 0; i < koll_buildings; i++)
+            for (int i = 0; i < numberOfBuildings ; i++)
             {
-                //запись характеристик зданий
+                //запись характеристик зданя
                 List<Characteristic> characteristics = new List<Characteristic>();
                 for (int j = 0; j < 13; j++)
                 {
-                    int poz = 1 + i * 14 + j;
+                    int position = 1 + i * 14 + j;
 
-                    Characteristic a = new Characteristic();
-                    a.Tipe = date[8][poz];
-
-                    try
-                    {
-                        a.Cost = int.Parse(date[9][poz]);
-                    }
-                    catch 
-                    {
-                        a.Cost = 0;
-                    }
-                    try
-                    {
-                        a.DestrIncome = int.Parse(date[10][poz]);
-                    }
-                    catch
-                    {
-                        a.DestrIncome = 0;
-                    }
-                    try
-                    {
-                        a.Heals = int.Parse(date[11][poz]);
-                    }
-                    catch
-                    {
-                        a.Heals = 0;
-                    }
-                    try
-                    {
-                        a.RangeAttack = int.Parse(date[12][poz]);
-                    }
-                    catch
-                    {
-                        a.RangeAttack = 0;
-                    }
-                    try
-                    {
-                        a.DamageSolders = new Damage(date[13][poz]);
-                    }
-                    catch
-                    {
-                        a.DamageSolders = new Damage("0-0");
-                    }
-                    try
-                    {
-                        a.DamageBuilding = new Damage(date[14][poz]);
-                    }
-                    catch
-                    {
-                        a.DamageBuilding = new Damage("0-0");
-                    }
-                    try
-                    {
-                        a.AttackSpeed = int.Parse(date[15][poz]);
-                    }
-                    catch
-                    {
-                        a.AttackSpeed = 0;
-                    }
-                    try
-                    {
-                        a.ReloadingAttack = int.Parse(date[16][poz]);
-                    }
-                    catch
-                    {
-                        a.ReloadingAttack = 0;
-                    }
-                    try
-                    {
-                        a.TimeTraining = float.Parse(date[17][poz]);
-                    }
-                    catch
-                    {
-                        a.TimeTraining = 0;
-                    }
-                    a.TipeAttack = convertStringToAttackType(date[18][poz]);
-                    a.Missile = convertStringToShellType(date[19][poz]);
-                    try
-                    {
-                        a.GoldIncome = int.Parse(date[20][poz]);
-                    }
-                    catch
-                    {
-                        a.GoldIncome = 0;
-                    }
-                    try
-                    {
-                        a.GoldIncomeReloading = int.Parse(date[21][poz]);
-                    }
-                    catch
-                    {
-                        a.GoldIncomeReloading = 0;
-                    }
-                    a.Bonus = date[22][poz];
-                    try
-                    {
-                        a.TimeBuild = float.Parse(date[23][poz]);
-                    }
-                    catch
-                    {
-                        a.TimeBuild = 0;
-                    }
-                    characteristics.Add(a);
+                    Characteristic BuildingCharacteristics = new Characteristic();
+                    BuildingCharacteristics.Tipe = date[8][position];
+                    BuildingCharacteristics.Cost = int.Parse(date[9][position]);
+                    BuildingCharacteristics.DestrIncome = int.Parse(date[10][position]);
+                    BuildingCharacteristics.Heals = int.Parse(date[11][position]);
+                    BuildingCharacteristics.RangeAttack = int.Parse(date[12][position]);
+                    BuildingCharacteristics.DamageSolders = new Damage(date[13][position]);
+                    BuildingCharacteristics.DamageBuilding = new Damage(date[14][position]);
+                    BuildingCharacteristics.AttackSpeed = int.Parse(date[15][position]);
+                    BuildingCharacteristics.ReloadingAttack = int.Parse(date[16][position]);
+                    BuildingCharacteristics.TimeTraining = float.Parse(date[17][position]);
+                    BuildingCharacteristics.TipeAttack = ConvertStringToAttackType(date[18][position]);
+                    BuildingCharacteristics.Missile = ConvertStringToShellType(date[19][position]);
+                    BuildingCharacteristics.GoldIncome = int.Parse(date[20][position]);
+                    BuildingCharacteristics.GoldIncomeReloading = int.Parse(date[21][position]);
+                    BuildingCharacteristics.Bonus = date[22][position];
+                    BuildingCharacteristics.TimeBuild = float.Parse(date[23][position]);
+                    characteristics.Add(BuildingCharacteristics);
                 }
 
-                //запись информации о здании
+                //Запись информации о здании
                 if (date[5][1+i * 14] != "" && date[5][1 + i * 14] != "-")
                 {
                     buildings.Add(new Building(date[1][1 + i * 14], date[2][1 + i * 14], date[4][1 + i * 14], true, characteristics));
@@ -219,12 +119,8 @@ namespace Практика
             }
 
             //запись рассы
-            Races races = new Races(date[0][1], buildings);
-
-            return races;
-
-
-
+            Race newRace = new Race(date[0][1], buildings);
+            return newRace;
         }
     }
 }
