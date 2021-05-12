7d61b51c12d2e4e5402792fda2aa78848161556b
STSRD <71692468+STSRD@users.noreply.github.com>
2021-05-12 18:56:25 +0300

    Add files via upload

---
+++ Unit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    struct Unit
+    {
+        public string Name;
+        public int AttackRange;
+        public float MovementSpeed;
+        public int AttackSpeed;
+        public float AttackCooldown;
+        public AttackType TypeAttack;
+        public List<UnitDescription> UnitDescriptions;
+
+        public Unit(
+        string Name,
+        int AttackRange,
+        int MovementSpeed,
+        int AttackSpeed,
+        float AttackCooldown,
+         AttackType TypeAttack,
+         List<UnitDescription> UnitDescriptions)
+        {
+            this.Name = Name;
+            this.AttackRange = AttackRange;
+            this.MovementSpeed = MovementSpeed;
+            this.AttackSpeed = AttackSpeed;
+            this.AttackCooldown = AttackCooldown;
+            this.TypeAttack = TypeAttack;
+            this.UnitDescriptions = UnitDescriptions;
+        }
+    }
+}
---
+++ UnitDescription.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    struct UnitDescription
+    {
+        public int Health;
+        public Damage UnitDamage;
+        public Damage BuildingDamage;
+        public string Skill;
+    }
+}
