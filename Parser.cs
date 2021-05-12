22e6aea959481954e88e2f96cfdbd4868e47df31
STSRD <71692468+STSRD@users.noreply.github.com>
2021-05-12 16:03:02 +0300

    Update Parser.cs

--- Parser.cs
+++ Parser.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Google.GData.Client;
-using Google.GData.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -83,44 +78,46 @@ namespace Practice
             for (int i = 0; i < numberOfBuildings ; i++)
             {
                 //запись характеристик зданя
-                List<Characteristic> characteristics = new List<Characteristic>();
+                List<BuildingDescription> buildingDescriptions = new List<BuildingDescription>();
                 for (int j = 0; j < 13; j++)
                 {
                     int position = 1 + i * 14 + j;
 
-                    Characteristic BuildingCharacteristics = new Characteristic();
-                    BuildingCharacteristics.Tipe = date[8][position];
-                    BuildingCharacteristics.Cost = int.Parse(date[9][position]);
-                    BuildingCharacteristics.DestrIncome = int.Parse(date[10][position]);
-                    BuildingCharacteristics.Heals = int.Parse(date[11][position]);
-                    BuildingCharacteristics.RangeAttack = int.Parse(date[12][position]);
-                    BuildingCharacteristics.DamageSolders = new Damage(date[13][position]);
-                    BuildingCharacteristics.DamageBuilding = new Damage(date[14][position]);
-                    BuildingCharacteristics.AttackSpeed = int.Parse(date[15][position]);
-                    BuildingCharacteristics.ReloadingAttack = int.Parse(date[16][position]);
-                    BuildingCharacteristics.TimeTraining = float.Parse(date[17][position]);
-                    BuildingCharacteristics.TipeAttack = ConvertStringToAttackType(date[18][position]);
-                    BuildingCharacteristics.Missile = ConvertStringToShellType(date[19][position]);
-                    BuildingCharacteristics.GoldIncome = int.Parse(date[20][position]);
-                    BuildingCharacteristics.GoldIncomeReloading = int.Parse(date[21][position]);
-                    BuildingCharacteristics.Bonus = date[22][position];
-                    BuildingCharacteristics.TimeBuild = float.Parse(date[23][position]);
-                    characteristics.Add(BuildingCharacteristics);
+                    BuildingDescription buildingDescription = new BuildingDescription();
+                    buildingDescription.Type = date[8][position];
+                    buildingDescription.Cost = int.Parse(date[9][position]);
+                    buildingDescription.SellIncome = int.Parse(date[10][position]);
+                    buildingDescription.Health = int.Parse(date[11][position]);
+                    buildingDescription.AttackRange = int.Parse(date[12][position]);
+                    buildingDescription.UnitDamage = new Damage(date[13][position]);
+                    buildingDescription.BuildingDamage = new Damage(date[14][position]);
+                    buildingDescription.AttackSpeed = int.Parse(date[15][position]);
+                    buildingDescription.AttackCooldown = int.Parse(date[16][position]);
+                    buildingDescription.RecruitTime = float.Parse(date[17][position]);
+                    buildingDescription.TypeAttack = ConvertStringToAttackType(date[18][position]);
+                    buildingDescription.Missile = ConvertStringToShellType(date[19][position]);
+                    buildingDescription.Income = int.Parse(date[20][position]);
+                    buildingDescription.GoldCooldown = int.Parse(date[21][position]);
+                    buildingDescription.Bonus = date[22][position];
+                    buildingDescription.BuildTime = float.Parse(date[23][position]);
+                    buildingDescriptions.Add(buildingDescription);
                 }
 
                 //Запись информации о здании
                 if (date[5][1+i * 14] != "" && date[5][1 + i * 14] != "-")
                 {
-                    buildings.Add(new Building(date[1][1 + i * 14], date[2][1 + i * 14], date[4][1 + i * 14], true, characteristics));
+                    buildings.Add(new Building(date[1][1 + i * 14], date[2][1 + i * 14], date[4][1 + i * 14], true, buildingDescriptions));
                 }
                 else {
-                    buildings.Add(new Building(date[1][1 + i * 14], date[2][1 + i * 14], date[4][1 + i * 14], false, characteristics));
+                    buildings.Add(new Building(date[1][1 + i * 14], date[2][1 + i * 14], date[4][1 + i * 14], false, buildingDescriptions));
                 }
             }
 
             //запись рассы
             Race newRace = new Race(date[0][1], buildings);
+
             return newRace;
         }
     }
 }
+
