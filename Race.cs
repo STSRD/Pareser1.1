1da9d298a7389f6c2c8fe1d721949d36b13203e3
STSRD <71692468+STSRD@users.noreply.github.com>
2021-05-12 18:57:25 +0300

    Update Race.cs

--- Race.cs
+++ Race.cs
@@ -8,10 +8,13 @@ namespace Practice
     {
         public string Name;
         public List<Building> Buildings;
+        public List<Unit> Units;
 
-        public Race(string name, List<Building> buildings) {
+        public Race(string name, List<Building> buildings, List<Unit> Units) {
             this.Name = name;
             this.Buildings = buildings;
+            this.Units = Units;
+
         }
 
     }
